d7fbc1fa29a2e82b965e7db1f6c36940f20b7dfa
Eason Wong <wangyatsing@gmail.com>
2022-02-01 17:59:49 +0100

    Add files via upload

---
+++ background.js
@@ -0,0 +1,69 @@
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {urlContains: 'theinitium.com/article'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(startup);
+
+chrome.tabs.onActivated.addListener(startup);
+
+function startup() {
+  var title = '';
+  var oldURL = '';
+  var response = '';
+  var article = '';
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    oldURL = tabs[0].url;
+    if (oldURL.includes('theinitium.com/article')){
+      console.log('Article page detected, starting up.');
+      chrome.tabs.sendMessage(tabs[0].id, {message: 'request_title'}, function(titleResponse) {
+        console.log('Title request sent.');
+        if (titleResponse != undefined){
+          title = titleResponse.value;
+          console.log('Title: ' + title);
+          console.log('URL: ' + URLConverter(title, oldURL, 0));
+          let xhr = new XMLHttpRequest();
+          xhr.open("GET", URLConverter(title, oldURL, 0), false);
+          xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+              xhrResponse = xhr.responseText;
+              console.log(xhrResponse);
+              if (xhrResponse.includes('<h1>404</h1>')){
+                chrome.tabs.sendMessage(tabs[0].id, {message: 'delete_button'}, function(r) {console.log('Content 404.');});
+              }else{
+                article = xhrResponse.substring(xhrResponse.indexOf('<p>'),xhrResponse.lastIndexOf('</p>',xhrResponse.lastIndexOf('</p>') - 1) + 4);
+                if (article.indexOf('<h4') > 0) article = article.substring(0, article.indexOf('<h4') - 1);
+                chrome.tabs.sendMessage(tabs[0].id, {message: article}, function(r) {console.log('Article sent to content script.');});
+              }
+            }else{
+              chrome.tabs.sendMessage(tabs[0].id, {message: 'delete_button'}, function(r) {console.log('Connection failed.');});
+            }
+          }
+          xhr.send();
+        }
+      });
+    }else if (oldURL.includes('theinitium.com')){
+      chrome.tabs.sendMessage(tabs[0].id, {message: 'delete_button'}, function(r) {console.log('Not article page.');});
+    }
+  });
+}
+
+function URLConverter(title, oldURL, x) {
+  let replace = ['，', '。', '、', '=', '⋯', '？', '！', '；' ,'：' ,'「', '」', '『', '』', '《', '》']; //need to be expanded
+  for (i of replace){
+    title = title.replaceAll(i,'');
+  }
+  let month = parseInt(oldURL[35] + oldURL[36]);
+  let date = parseInt(oldURL[37] + oldURL[38]) + x; //need to be upgraded
+  if (month.toString().length == 1) month = '0' + month.toString();
+  if (date.toString().length == 1) date = '0' + date.toString();
+  return 'https://telegra.ph/' + title + '端傳媒-Initium-Media-' + month + '-' + date;
+}
+
---
+++ content_script.js
@@ -0,0 +1,27 @@
+
+var article = 'null';
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if (request.message == 'request_title'){
+			sendResponse({value: document.getElementsByClassName('headline u-font-serif')[0].innerText});
+		}else if (request.message == 'delete_button'){
+			let button = document.getElementById('intium_unlocker');
+			if (buttonExists()) button.parentNode.removeChild(button);
+		}else if (!buttonExists() && document.getElementsByClassName('paywall').length != 0){
+			article = request.message;
+			let buttons = document.getElementsByClassName('buttons');
+			buttons[0].insertAdjacentHTML('afterend','<button class="buttons" id="intium_unlocker"><a class="btn">免費支持</a></button>');
+			document.getElementById('intium_unlocker').addEventListener("click", unlock);
+		}
+	}
+);
+
+function unlock() {
+	console.log('clicked.');
+	document.getElementsByClassName("article__body")[0].innerHTML = article;
+}
+
+function buttonExists() {
+	return document.getElementById('intium_unlocker') != null;
+}
