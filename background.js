772fff3baaff50dce0fb888a6ad1cc0e576c8c81
Eason Wong <wangyatsing@gmail.com>
2022-07-23 22:44:44 +0200

    Update v1.3

--- background.js
+++ background.js
@@ -1,57 +1,61 @@
 
 browser.tabs.onUpdated.addListener(startup);
-
 browser.tabs.onActivated.addListener(startup);
 
 function startup() {
-  var title = "";
-  var oldURL = "";
-  var article = "";
-  browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    oldURL = tabs[0].url;
-    if (oldURL.includes("theinitium.com/article")) {
-      console.log("Article page detected, starting up.");
-      browser.tabs.sendMessage(tabs[0].id, { message: "request_title" }, (titleResponse) => {
-        console.log("Title request sent.");
-        if (titleResponse != undefined) {
-          title = titleResponse.value;
-          console.log("Title: " + title);
-          console.log("URL: " + URLConverter(title, oldURL, 0));
-          let xhr = new XMLHttpRequest();
-          xhr.open("GET", URLConverter(title, oldURL, 0), false);
-          xhr.onreadystatechange = () => {
-            if (xhr.readyState == 4) {
-              xhrResponse = xhr.responseText;
-              console.log(xhrResponse);
-              if (xhrResponse.includes("<h1>404</h1>")) {
-                browser.tabs.sendMessage(tabs[0].id, { message: "delete_button" }, () => { console.log("Content 404."); });
-              } else {
-                article = xhrResponse.substring(xhrResponse.indexOf("<p>"), xhrResponse.lastIndexOf("</p>", xhrResponse.lastIndexOf("</p>") - 1) + 4);
-                if (article.indexOf("<h4") > 0)
-                  article = article.substring(0, article.indexOf("<h4") - 1);
-                browser.tabs.sendMessage(tabs[0].id, { message: article }, () => { console.log("Article sent to content script."); });
-              }
-            } else {
-              browser.tabs.sendMessage(tabs[0].id, { message: "delete_button" }, () => { console.log("Connection failed."); });
-            }
-          };
-          xhr.send();
-        }
-      });
-    } else if (oldURL.includes("theinitium.com")) {
-      browser.tabs.sendMessage(tabs[0].id, { message: "delete_button" }, () => { console.log("Not article page."); });
-    }
-  });
+  browser.tabs.query({ active: true }, tabs => tabs.forEach(tab => scanEachTab(tab)));
 }
 
-function URLConverter(title, oldURL, x) {
-  let replace = ["，", "。", "、", "=", "⋯", "？", "！", "；", "：", "︰", "（", "）", "「", "」", "『", "』", "《", "》", ",", ".", ":", ";", "—", "【", "】"]; //need to be expanded
-  for (i of replace) {
-    title = title.replaceAll(i, "");
+function scanEachTab(tab) {
+  let url = tab.url;
+  if (url.includes("theinitium.com/article")) {
+    console.log("Original URL: " + url + "\nArticle page detected, start searching for article source.");
+    browser.tabs.sendMessage(tab.id, "request_title", title => {
+      title ? searchForArticle(title, url, tab, 0) : console.error("Can't get article title. Maybe the article isn't loaded.");
+    });
+  } else if (url.includes("theinitium.com")) {
+    browser.tabs.sendMessage(tab.id, "delete_button", () => { console.log("Not article page."); });
   }
-  title = title.replaceAll(" ", "-"); //mystery dash needs to be solve
-  let month = parseInt(oldURL[35] + oldURL[36]);
-  let date = parseInt(oldURL[37] + oldURL[38]) + x; //need to be upgraded
+}
+
+function searchForArticle(title, url, tab, i) {
+  console.log("Article title: " + title + "\nArticle URL: " + URLConverter(title, url, i));
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", URLConverter(title, url, i), false);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) { //HTML request fullfilled
+      let html_text = xhr.responseText;
+      if (html_text.includes("<h1>404</h1>") || !html_text.includes("@chotamreaderbot")) {
+        browser.tabs.sendMessage(tab.id, "delete_button", () => {
+          if (i < 3) {
+            console.log("Attempt: " + i + "\nContent unavailable.");
+            searchForArticle(title, url, tab, ++i);
+          } else {
+            console.log("Too many attempts, giving up.");
+          }
+        });
+      } else { //article extracted successfully
+        article = html_text.substring(html_text.indexOf("<p>"), html_text.lastIndexOf("</p>", html_text.lastIndexOf("</p>") - 1) + 4);
+        if (article.indexOf("<h4") > 0) article = article.substring(0, article.indexOf("<h4") - 1); //Get rid of a banner
+        browser.tabs.sendMessage(tab.id, article, () => { console.log("Article sent to content script."); });
+      }
+    } else {
+      browser.tabs.sendMessage(tab.id, "delete_button", () => { console.log("Connection failed."); });
+    }
+  };
+  xhr.send();
+}
+
+function URLConverter(title, url, i) {
+  let replace = ["，", "。", "、", "=", "⋯", "？", "！", "；", "：", "︰", "（", "）", "「", "」", "『", "』", "《", "》", ",", ".", ":", ";", "—", "【", "】"];
+  for (symbol of replace) title = title.replaceAll(symbol, "");
+  title = title.replaceAll(" ", "-");
+  let month = parseInt(url[35] + url[36]);
+  let date = parseInt(url[37] + url[38]) + i;
+  if (date > 31) {
+    month++;
+    date = date - 31;
+  }
   if (month.toString().length == 1) month = "0" + month.toString();
   if (date.toString().length == 1) date = "0" + date.toString();
   return "https://telegra.ph/" + title + "端傳媒-Initium-Media-" + month + "-" + date;
--- content_script.js
+++ content_script.js
@@ -1,15 +1,15 @@
 
-var article = "null";
+var article = "";
 
 browser.runtime.onMessage.addListener(
-	(request, sender, sendResponse) => {
-		if (request.message == "request_title") {
-			sendResponse({ value: document.getElementsByClassName("headline u-font-serif")[0].innerText });
-		} else if (request.message == "delete_button") {
+	(message, sender, sendResponse) => {
+		if (message == "request_title") {
+			sendResponse(document.getElementsByClassName("headline u-font-serif")[0].innerText);
+		} else if (message == "delete_button") {
 			let button = document.getElementById("intium_unlocker");
 			if (buttonExists()) button.parentNode.removeChild(button);
 		} else if (!buttonExists() && document.getElementsByClassName("paywall").length != 0) {
-			article = request.message;
+			article = message;
 			let buttons = document.getElementsByClassName("buttons");
 			buttons[0].insertAdjacentHTML("afterend", '<button class="buttons" id="intium_unlocker"><a class="btn">免費支持</a></button>');
 			document.getElementById("intium_unlocker").addEventListener("click", unlock);
@@ -18,7 +18,6 @@ browser.runtime.onMessage.addListener(
 );
 
 function unlock() {
-	console.log("clicked.");
 	document.getElementsByClassName("article__body")[0].innerHTML = article;
 }
 
